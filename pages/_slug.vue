33fcdc87d9bcb6c81165514ca861b0759be3e640
Sandip Baradiya <baradiya.sandip@gmail.com>
2020-11-09 00:36:26 +0530

    added sample slug page

---
+++ pages/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="text-center text-2xl">
+    slug: {{slug}}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'sample-slug',
+    asyncData(ctx){
+        const {slug} = ctx.params
+        return {
+            slug
+        }
+    },
+
+
+    data: () => ({}),
+
+    methods: {},
+}
+</script>
+<style lang="postcss" scoped>
+</style>
